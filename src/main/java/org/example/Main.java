6ef6ca0207b5d76bfbd180bdcf726361e4ebed26
andr777 <andr.root@gmail.com>
2022-10-30 12:41:54 +0300

    домашнее задание урока 2.15. Основы алгоритмов, часть 2.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        IntegerList nums = new IntegerListImpl(100_000);
+
+        nums.sort();
     }
 }
